7916ab7dd5f9a855e3b0706c026196555f19f673
wirickad <wirickad@gmail.com>
2023-01-23 08:38:55 -0700

    Add project files.

---
+++ Driver/Driver.cs
@@ -0,0 +1,20 @@
+﻿/*1) The “Driver” class (the class with the main method where the program starts) will:
+• Welcome the user to the game
+• Create a game board array to store the players’ choices
+• Ask each player in turn for their choice and update the game board array
+• Print the board by calling the method in the supporting class
+• Check for a winner by calling the method in the supporting class, and notify the players
+when a win has occurred and which player won the game */
+
+using System;
+
+namespace Driver
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Driver/Support.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*2) The supporting class will:
+• Receive the “board” array from the driver class
+• Contain a method that prints the board based on the information passed in
+• Contain a method that receives the game board array as input and returns if there is a
+winner and who it was*/
+
+namespace Driver
+{
+    class Support
+    {
+
+    }
+}
