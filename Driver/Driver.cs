3306280cd06acec22588235eefe02cdae5c6dbd0
wirickad <wirickad@gmail.com>
2023-01-23 12:20:40 -0700

    test

--- Driver/Driver.cs
+++ Driver/Driver.cs
@@ -14,7 +14,90 @@ namespace Driver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initialize array and positions 
+            string[] arrBoard = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+            int Player1Position = 0;
+            int Player2Position = 0;
+            string strResults = null;
+
+            Console.WriteLine("Welcome to the game, playa!");
+            Support st = new Support();
+            st.PrintBoard(arrBoard);
+
+            //Loop through the game until there is a winner
+            while (strResults == null)
+            {
+
+                Console.WriteLine("\n" + "Player 1, enter a number (0-8) for where you want to place your X: ");
+                Player1Position = int.Parse(Console.ReadLine());
+
+                //Check to see if the position has already been taken
+                if (arrBoard[Player1Position] == Convert.ToString(Player1Position))
+                {
+                    arrBoard[Player1Position] = Char.ToString('O');
+                }
+                else
+                {
+                    //If the position is already taken, loop through until a valid position is chosen
+                    while (arrBoard[Player1Position] != Convert.ToString(Player1Position))
+                    {
+                        Console.WriteLine("That spot is already taken. Please enter a valid position: ");
+                        Player1Position = int.Parse(Console.ReadLine());
+                    }
+
+                    arrBoard[Player1Position] = Char.ToString('O');
+                }
+
+                //Check to see if there is a winner and display the Board
+                strResults = st.ReceiveBoard(arrBoard);
+                st.PrintBoard(arrBoard);
+
+                //Break out of the loop if there is a winner
+                if (strResults != null)
+                {
+                    break;
+                }
+
+                Console.WriteLine("\nPlayer 2, enter a number (0-8) for where you want to place your O: ");
+                Player2Position = int.Parse(Console.ReadLine());
+
+                //Check to see if the position has already been taken
+                if (arrBoard[Player2Position] == Convert.ToString(Player2Position))
+                {
+                    arrBoard[Player2Position] = Char.ToString('O');
+                }
+                else
+                {
+                    //If the position is already taken, loop through until a valid position is chosen
+                    while (arrBoard[Player2Position] != Convert.ToString(Player2Position))
+                    {
+                        Console.WriteLine("That spot is already taken. Please enter a valid position: ");
+                        Player2Position = int.Parse(Console.ReadLine());
+                    }
+
+                    arrBoard[Player2Position] = Char.ToString('O');
+                }
+
+
+                //Check to see if there is a winner and display the Board
+                strResults = st.ReceiveBoard(arrBoard);
+                st.PrintBoard(arrBoard);
+            }
+
+
+            //Declare a winner
+            if (strResults == "Player1")
+            {
+                Console.WriteLine("Player1 has won the game!");
+            }
+            else if (strResults == "Player2")
+            {
+                Console.WriteLine("Player2 has won the game!");
+            }
+            else
+            {
+                Console.WriteLine("The game has ended in a tie!");
+            }
         }
     }
 }
--- Driver/Support.cs
+++ Driver/Support.cs
@@ -9,8 +9,85 @@ winner and who it was*/
 
 namespace Driver
 {
-    class Support
+    public class Support
     {
+        public void PrintBoard(string[] Board)
+        {
+            Console.WriteLine("    {0}  |   {1}   |   {2}", Board[0], Board[1], Board[2]);
+            Console.WriteLine("--------------------");
+            Console.WriteLine("    {0}  |   {1}   |   {2}", Board[3], Board[4], Board[5]);
+            Console.WriteLine("--------------------");
+            Console.WriteLine("    {0}  |   {1}   |   {2}", Board[6], Board[7], Board[8]);
 
+        }
+        public string ReceiveBoard(string[] Board)
+        {
+            
+            string winner = "";
+            
+            
+           
+               if (Board[0] == Board[3] && Board[3] == Board[6])
+                    {
+                        winner = Board[0];
+                    }
+               else if (Board[0] == Board[1] && Board[1] == Board[2])
+                    {
+                        winner = Board[0];
+                    }
+               else if (Board[0] == Board[4] && Board[4] == Board[8]) 
+                    {
+                        winner = Board[0];
+                    }
+               else if (Board[3] == Board[4] && Board[4] == Board[5])
+                    {
+                        winner = Board[3];
+                    }
+               else if (Board[6] == Board[7] && Board[7] == Board[8])
+                    {
+                        winner  = Board[6];
+                    }
+               else if (Board[1] == Board[4] && Board[4] == Board[7])
+                    {
+                        winner  = Board[1];
+                    }
+               else if (Board[2] == Board[4] && Board[4] == Board[6])
+                    {
+                         winner  = Board[2];
+                    }
+               else if (Board[2] == Board[5] && Board[5] == Board[8])
+                    {
+                         winner  = Board[2];
+                    }
+               else
+                    {
+                        winner = null;
+                    }
+            if (winner == "X")
+            {
+                winner = "Player 1";
+            }
+            else if (winner == "O")
+            {
+                winner = "Player 2";
+            }
+            else
+            {
+                winner = null;
+            }
+            for (int i = 0; i < Board.Length; i++)
+            {
+                if (Board[i] != "X" || Board[i] != "O")
+                {
+                    Console.Write("kjlkj");
+                    winner = "Draw";
+                    return winner;
+                }
+            }
+
+            PrintBoard(Board);
+            return winner;
+                
+        }
     }
 }
